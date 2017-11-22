59b66f35a34fb68f600d534548bb4fbe2f3a3344
Miss-Terra <ms.terra.h@gmail.com>
2017-11-21 23:11:52 -0800

    kay

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const
 
   let eventList;
 
-// Sets server port and logs message on success
+// Sets server port and logs message on success/
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
