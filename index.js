4b6e663e6366323a4ecb2485687a17e161baf94d
Miss-Terra <ms.terra.h@gmail.com>
2017-11-22 22:56:27 -0800

    dsg

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ app.get('/getEvents', function (req, output) {
       return;
     }
     eventList = res.data;
+    delete eventList["paging"];
     let response = JSON.stringify(res);
     updateFile(response);
     output.send(response);
